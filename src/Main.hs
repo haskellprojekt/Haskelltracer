30cd18c7f89093eded05fafc31e848c634a68449
Kai Lüke <pothos@riseup.net>
2012-01-12 12:20:40 +0100

    Anderes Konzept für Scene

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,12 @@ import Haskelltracer
 import Plane
 import Triangle
 
--- hardcoded because even with some extended tricks a mixed list is not possibl$
+data Geometries = T Triangle | P Plane deriving Show
+-- hardcoded because even with some extended tricks a mixed list is not possible
+
 data Scene = Scene { camera :: Camera
-,   sPlane :: [Plane]
-,   sTriangle :: [Triangle]
+,   sGeometries :: [Geometries]
 }
 
 main = do
-       -- folgendes geht nicht...
-       --let weltliste = [] :: [Geometry g => g]
-       --let welt = ((Triangle (0,0,0) (0,0,0) (0,0,0)) :: Geometry b => b):weltliste
-       --let d = Triangle (0,0,0) (0,0,0) (0,0,0)
-       --let p = Plane (0,0,0) (0,0,0) (0,0,0) (0,0,0)
-       --let w = ((d :: Geometry g => g):[p])
        putStrLn "Hello World!"
