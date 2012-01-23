1b464b994a4e9c0a15135875bdb1d5e8f6a81597
yves <steve.harrison@gmx.net>
2012-01-23 14:48:58 +0100

    reimplmented parser, still issues with whitespaces

--- src/parser.hs
+++ src/parser.hs
@@ -1,10 +1,19 @@
+import Monad
 import Text.ParserCombinators.Parsec
 import qualified Text.ParserCombinators.Parsec.Token as P
 import Text.ParserCombinators.Parsec.Language (haskellDef)
 
 
-data Vector = Vector Double Double Double deriving Show
-data Object = Sphere Vector Float | Camera Vector Vector deriving Show
+--data Vector = Vector Double Double Double deriving Show
+--data Object = Sphere Vector Float | Camera Vector Vector deriving Show
+
+type Vector =  (Float, Float, Float)
+data Prim = Axis Char | Number Float | Vector Vector deriving Show
+type Option = String
+
+data OO = Obj Object | Opt Option deriving Show
+
+data Object = Object String [Prim] [OO] deriving Show
 
 povFile :: GenParser Char st [Object]
 povFile =
@@ -12,6 +21,70 @@ povFile =
        eof
        return result
 
+object :: GenParser Char st Object
+object =
+    do
+        name <- many (noneOf " \n{")
+        spaces
+        char '{'
+        params <- sepBy parameter (char ',')
+        --opts <- sepBy optionOrObject (char '\n')
+        char '}'
+        return (Object name params [])
+        --return (Object name params opts)
+
+parameter :: GenParser Char st Prim
+parameter =
+    do  l <- letter; return (Axis l)
+    <|>
+    do  d <- (Number . parseNumber);
+--      return (Number d)
+        return d
+--   <|>
+--    ( Vector $ vector )
+
+-- doesn't work for doubles
+parseNumber :: GenParser Char st Float
+parseNumber = liftM read $ many1 digit
+
+-- vector :: GenParser Char st Vec
+--vector = do
+--    char '<'; x <- parseNumber
+--    char ','; y <- parseNumber
+--    char ','; z <- parseNumber
+--    char '>'; return (x,y,z)
+
+
+option1 :: GenParser Char st String
+option1 =
+    do
+        opt <- (many (noneOf "\n"))
+        return opt
+
+optionOrObject :: GenParser Char st OO
+optionOrObject =
+    do
+        obj <- try ( object )
+        return (Obj obj)
+    <|>
+    do
+        opt <- option1
+        return (Opt opt)
+
+
+
+
+
+parsePov :: String -> Either ParseError [Object]
+parsePov input = parse povFile "(unknown)" input
+
+parsePovFile =
+    do
+        content <- readFile "../test.pov"
+        return (parsePov content)
+
+----------------------------------------------
+{-
 lexer   = P.makeTokenParser haskellDef
 
 object =
@@ -50,6 +123,4 @@ vector =
         (x:y:z:[]) <- sepBy (P.float lexer) (char ',')
         char '>'
         return (Vector x y z)
-
-parsePov :: String -> Either ParseError [Object]
-parsePov input = parse povFile "(unknown)" input
+-}
